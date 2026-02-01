5b0e6fa428d7deb865cf7ceaac51a096955c3f54
遂沫 <1992724048@qq.com>
2026-02-02 01:25:01 +0800

    添加flutter作为设置界面

--- CPP/hwopt/hwopt/dllmain.cpp
+++ CPP/hwopt/hwopt/dllmain.cpp
@@ -23,7 +23,6 @@ auto APIENTRY DllMain(HMODULE hModule, const DWORD ul_reason_for_call, LPVOID lp
             JavaNativeBase::init_all();
             break;
         case DLL_THREAD_ATTACH:
-            thread_local auto _ = _set_se_translator(stdpp::exception::NativeException::seh_to_ce);
             break;
         case DLL_THREAD_DETACH:
             break;
@@ -45,8 +44,6 @@ extern "C" API auto JAVA_ResolveFunction(const char* name) -> void* {
                            }
                        }
                    });
-
-    thread_local auto _ = _set_se_translator(stdpp::exception::NativeException::seh_to_ce);
     if (const auto opt = JavaNativeBase::get_method(name)) {
         return opt.value();
     }
